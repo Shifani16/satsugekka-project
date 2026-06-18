63041c7d25340d2d07fd6409600679f4fe4dd0bd
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2026-06-18 23:36:24 +0700

    fixing login (2)

--- client/src/component/admin/login.tsx
+++ client/src/component/admin/login.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     const baseUrl = import.meta.env.VITE_API_URL
 
     try {
-      const res = await fetch(`${baseUrl}/api/login`, {
+      const res = await fetch(`${baseUrl}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
